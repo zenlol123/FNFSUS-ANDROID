e450c2eb688d88e8f5e2c233e195b463ceac7ea9
TheOrda <91075908+TheOrda@users.noreply.github.com>
2022-02-17 16:04:57 -0300

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -148,6 +148,15 @@ class MainMenuState extends MusicBeatState
 
 		super.create();
 	}
+	
+#if ACHIEVEMENTS_ALLOWED
+	// Unlocks "Freaky on a Friday Night" achievement
+	function giveAchievement() {
+		add(new AchievementObject('friday_night_play', camAchievement));
+		FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+		trace('Giving achievement "friday_night_play"');
+	}
+	#end
 
 	var selectedSomethin:Bool = false;
 
